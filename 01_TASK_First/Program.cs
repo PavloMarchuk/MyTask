fe5ee5ea8393cfeea7a8884e169fa4d641f78d77
Pavlo Marchuk <Pavlo Marchuk@DESKTOP-MQBICKJ>
2017-06-12 09:47:44 +0300

    TASKs

--- 01_TASK_First/Program.cs
+++ 01_TASK_First/Program.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _01_TASK_First
 	{
 	class Program
 		{
+	
+			static void MyTask()
+			{
+			Thread.CurrentThread.IsBackground = false;
+			Console.WriteLine("MyTask() запущен.");			
+			for(int count = 0; count < 20; count++)
+				{
+				Thread.Sleep(500);
+				Console.WriteLine("В методе MyTask(), счетчик равен: " + count);
+				}
+
+			Console.WriteLine("MyTask() завершен.");
+			}
 		static void Main(string[] args)
 			{
+			Task task = new Task(MyTask);
+			task.Start();
+			Thread.Sleep(10);
+			//Thread.Sleep(2000);
+			Console.WriteLine("Main thread END!");
 			}
 		}
 	}
---
+++ 02_TaskId/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02_TaskId
+	{
+	class Program
+		{
+		static void MyTask()
+			{
+			//Thread.CurrentThread.IsBackground = false;
+			Console.WriteLine($"MyTask() запущен. ThreadID = {Task.CurrentId}");
+			for(int count = 0; count < 10; count++)
+				{
+				Thread.Sleep(500);
+				Console.WriteLine($"В методе MyTask({Task.CurrentId}), счетчик равен: {count}"  );
+				}
+			Console.WriteLine("MyTask() завершен.");
+			}
+		static void Main(string[] args)
+			{			
+			Console.WriteLine($"Thread main - CurrentID = {Task.CurrentId}");
+			
+
+			Task task1 = new Task(MyTask);			
+			Task task2 = new Task(MyTask);
+			task1.Start();
+			task2.Start();
+
+			Console.WriteLine($"task1 ID={task1.Id}");
+			Console.WriteLine($"task2 ID={task2.Id}");
+
+			Console.ReadKey();
+			}
+		}
+	}
